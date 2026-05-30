6aaf17979bff867d2edfd783dc9c20751a540e6d
hasungkr <hasunglee132@gmail.com>
2026-05-30 16:57:03 +1200

    add alphabet list & add columns calculation

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const canvas = document.getElementById('matrix');
 const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 16;
+const columns = canvas.width/fontSize;
